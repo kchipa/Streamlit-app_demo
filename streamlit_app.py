10de1287f94c7ea796a02806c0291839db036234
Konstantine <139650697+kchipa@users.noreply.github.com>
2024-12-20 23:29:49 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,17 @@ crypto_stocks_prediction_app = st.Page(
     title="Crypto Stocks Prediction App",
     icon = ":material/linked_services:",
 )
-movie_recommendation_app = st.Page(
-    page='views/crypto_stocks_prediction_app.py',
-    title="Crypto Stocks Prediction App",
-    icon = ":material/linked_services:",
-)
+#movie_recommendation_app = st.Page(
+ #   page='views/crypto_stocks_prediction_app.py',
+  #  title="Crypto Stocks Prediction App",
+   # icon = ":material/linked_services:",
+#)
 
 #pg = st.navigation(pages=[about_page, crypto_stocks_prediction_app])
 pg = st.navigation(
     {
         "Info": [about_page],
-        "Projects": [crypto_stocks_prediction_app, movie_recommendation_app],
+        "Projects": [crypto_stocks_prediction_app],
     }
 )
 
