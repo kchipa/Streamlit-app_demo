315ded925b7d0cbae9d97e3ee316f00c74ce955b
Konstantine <k.chipashvili03@gmail.com>
2024-12-20 22:31:19 +0400

    iniatial Commit

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import streamlit as st 
+
+# --- Page Setup ---
+about_page = st.Page(
+    page='views/about_me.py',
+    title="About Me",
+    icon =":material/account_circle:",
+    default=True,
+)
+crypto_stocks_prediction_app = st.Page(
+    page='views/crypto_stocks_prediction_app.py',
+    title="Crypto Stocks Prediction App",
+    icon = ":material/linked_services:",
+)
+movie_recommendation_app = st.Page(
+    page='views/crypto_stocks_prediction_app.py',
+    title="Crypto Stocks Prediction App",
+    icon = ":material/linked_services:",
+)
+
+#pg = st.navigation(pages=[about_page, crypto_stocks_prediction_app])
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [crypto_stocks_prediction_app, movie_recommendation_app],
+    }
+)
+
+st.sidebar.text("Made by Kosta")
+pg.run()
---
+++ views/about_me.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from forms.contact import contact_form
+
+@st.dialog("Contact Me")
+def show_contact_form():
+    st.write("Email: k.chipashvili03@gmail.com")
+    contact_form()
+
+
+col1, col2 = st.columns(2, gap="small", vertical_alignment='center')
+with col1:
+    st.image("./assets/1687373508577.jpg", width=230)
+with col2:
+    st.title("Konstantine Chipashvili", anchor=False)
+    st.write(
+        "Junior Data Analyst"
+    )
+    if st.button("Contact Me"):
+        show_contact_form()
+
+# ---Skills ---
+st.write("\n")
+st.subheader("Hard Skills", anchor=False)
+st.write(
+    '''
+    - Programming: Python (pandas, numpy, Scikit-learn) and SQL
+    - Data Visualisation: Matplotlib, Seaborn, PowerBi, MS Excel
+    - Databases: Postgres
+    '''
+)
